fcec55b18b611deea4eed1fd7d2dc79335007963
Your Name <2120201202@mail.nankai.edu.cn>
2023-04-09 14:53:08 +0800

    first commit

---
+++ example.py
@@ -0,0 +1,16 @@
+from PyaiVS import model_bulid,virtual_screen
+model_bulid.running('./dataset/abcg2.csv',
+                    out_dir='./dataset',
+                    run_type='param',
+                    cpus=4)
+model_bulid.running('./dataset/abcg2.csv',
+                    out_dir='./dataset',
+                    run_type='result',
+                    cpus=4)
+virtual_screen.model_screen(model='SVM',
+                            split='random',
+                            FP='MACCS',
+                            model_dir='./dataset/abcg2/model_save',
+                            screen_file='./database/base.csv',
+                            sep=';',
+                            smiles_col='smiles')
